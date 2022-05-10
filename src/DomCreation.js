a02d3e6654f0d234345f4f95c189f1d67e356624
Raymond Chen <raychen358@gmail.com>
2022-05-09 21:02:39 -0700

    Created module for header with icon

---
+++ src/DomCreation.js
@@ -0,0 +1,20 @@
+const header = () => {
+    //Creates Header Elements 
+    const header = document.createElement('div');
+    header.classList.add('header');
+
+    const span = document.createElement('span');
+    span.classList.add("material-symbols-outlined");
+    span.textContent = 'event_avilable'
+    
+    const headText = document.createElement('div');
+    headText.classList.add('head-text');
+    headText.textContent = 'TO-DO List';
+
+    document.body.appendChild(header);
+    header.appendChild(span);
+    header.appendChild(headText);
+
+}; header();
+
+export default header
